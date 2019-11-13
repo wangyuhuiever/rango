b062112eb8b9ab3dffad17e847a045226fe67697
Yuhui <yuhui.wang@openstone.cn>
2019-11-13 17:42:08 +0800

    [ADD]complete chapter 1 exercise

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('balabalabala')
+    return HttpResponse('balabalabala<br/><a href="/rango/about/">about page</a>')
+
+
+def about(request):
+    return HttpResponse('about page<br/><a href="/rango/">index page</a>')
