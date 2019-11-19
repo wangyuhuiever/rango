71e209846b11280f3ff1a15fe830e40f9a4e92af
Yuhui <yuhui.wang@openstone.cn>
2019-11-19 11:02:04 +0800

    [ADD]complete chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+
+class PageInLine(admin.StackedInline):
+    model = Page
+    extra = 3
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [PageInLine]
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
